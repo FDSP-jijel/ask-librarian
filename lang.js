00ca967254346db6c24d4dffd0cedaf923d626cb
FDSP-jijel <boumazanora17@gmail.com>
2026-04-12 13:41:14 +0100

    Update lang.js

--- lang.js
+++ lang.js
@@ -2,18 +2,17 @@
 function setLanguage(lang){
 
     document.querySelectorAll('[data-lang]').forEach(el => {
-        el.style.display = (el.getAttribute('data-lang') === lang) ? 'inline' : 'none';
+        el.style.display = (el.getAttribute('data-lang') === lang) ? '' : 'none';
     });
 
-    // حفظ اللغة المختارة (اختياري لكنه مهم)
+    // حفظ اللغة في المتصفح
     localStorage.setItem('lang', lang);
 }
 
 // ================== تشغيل تلقائي عند فتح الصفحة ==================
 document.addEventListener('DOMContentLoaded', () => {
 
-    // استرجاع اللغة المحفوظة أو العربية افتراضياً
     let savedLang = localStorage.getItem('lang') || 'ar';
-
     setLanguage(savedLang);
+
 });
