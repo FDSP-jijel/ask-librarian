27f6cc91d456d92178ce6417d7ddd07712ad860d
FDSP-jijel <boumazanora17@gmail.com>
2026-04-13 18:54:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -323,6 +323,13 @@ function setLanguage(lang){
 
 function sendMessage(){
 
+   messages.push({
+    name,
+    email,
+    message,
+    time: new Date().toLocaleTimeString()
+});
+   
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
     let message = document.getElementById("message").value.trim();
