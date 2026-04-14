97a59876bfa8fb7296f3faf1f9f7e17323ebf292
FDSP-jijel <boumazanora17@gmail.com>
2026-04-14 03:12:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -508,3 +508,22 @@ function drawPieChart(){
 
     pie = new Chart(ctx, config);
 }
+
+function animateValue(id, start, end, duration){
+
+    let obj = document.getElementById(id);
+    let range = end - start;
+    let stepTime = Math.abs(Math.floor(duration / range));
+
+    let current = start;
+
+    let timer = setInterval(() => {
+        current++;
+        obj.textContent = current;
+
+        if(current >= end){
+            clearInterval(timer);
+        }
+
+    }, stepTime);
+}
