6f019382fe026c784df00e705e23e3ba1d46d6c3
FDSP-jijel <boumazanora17@gmail.com>
2026-04-26 10:01:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -189,7 +189,10 @@ currentResults = currentResults.filter(row => {
     let words = query.split(" ");
 
 let strongMatch = words.some(word => {
-    let regex = new RegExp(`\\b${word}\\b`, "i");
+    if(/[a-z]/i.test(word)){
+        let regex = new RegExp(`\\b${word}\\b`, "i");
+        return regex.test(text);
+    }
     return text.includes(word);
 });
 
