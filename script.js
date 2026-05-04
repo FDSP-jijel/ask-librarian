ff0836f5ce633ca6b81f01564fddc52c7ecc8d5e
FDSP-jijel <boumazanora17@gmail.com>
2026-05-04 09:30:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -967,6 +967,12 @@ function loadMessages(){
   `;
 }
 
+function test(){
+    console.log("JS يعمل بشكل طبيعي");
+}
+test();
+
+
 window.show = show;
 window.searchBooks = searchBooks;
 window.startVoice = startVoice;
